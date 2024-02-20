6f04e7366d7dcafdd31e8c684393bb259fb5f919
Nata test <verba16@yandex.ru>
2024-02-20 20:15:21 +0300

    first commit

---
+++ src/test/java/ru/netology/cardDeliveryDate/data/DataGenerator.java
@@ -0,0 +1,65 @@
+
+package ru.netology.cardDeliveryDate.data;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[] {
+                "Москва", "Ростов-на-Дону", "Омск", "Ярославль", "Челябинск"
+        };
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo();
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+    }
+    }
