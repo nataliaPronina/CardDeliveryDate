64101e64c57c0ff363d7c23dcaae77d83bef7f84
Nata test <verba16@yandex.ru>
2024-03-08 13:24:47 +0300

    test changed

--- src/test/java/ru/netology/cardDeliveryDate/test/CardDeliveryDateTest.java
+++ src/test/java/ru/netology/cardDeliveryDate/test/CardDeliveryDateTest.java
@@ -27,6 +27,7 @@ class CardDeliveryDateTest {
         String firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         int daysToAddForSecondMeeting = 7;
         String secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
         $("[data-test-id=city] input").setValue(validUser.getCity());
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(firstMeetingDate);
@@ -34,17 +35,21 @@ class CardDeliveryDateTest {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
-        $("[data-test-id='success-notification']")
-                .shouldBe(Condition.visible, Duration.ofSeconds(20));
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(Condition.exactText(
+                        "Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(Condition.visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
-        $("[data-test-id='replan-notification'] .notification_content")
+        $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(Condition.visible);
         $("[data-test-id='replan-notification'] button").click();
-        $("[data-test-id='success-notification'].notification_content")
-                .shouldHave(Condition.exactText("Встреча успешно запланирована на" + secondMeetingDate))
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(Condition.visible);
     }
 }
